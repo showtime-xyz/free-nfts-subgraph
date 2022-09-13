94733a76c1767c961053a663b27ff3ee4885059c
karmacoma <karma@coma.lol>
2022-09-12 17:13:48 -0700

    Fix receipt parsing issues
    - first enable receipts in subgraph.yml
    - they require apiVersion >= 0.0.7
    - work around weird string equality issues, so use string.include() instead
    - not sure address comparison is supported, so do another string include check

--- src/single-edition-mintable-creator.ts
+++ src/single-edition-mintable-creator.ts
@@ -1,36 +1,31 @@
-import { Address, BigInt, Bytes, log } from "@graphprotocol/graph-ts";
+import { Address, BigInt, Bytes, ethereum, log } from "@graphprotocol/graph-ts";
 import { CreatedEdition as CreatedEditionEvent } from "../generated/SingleEditionMintableCreator/SingleEditionMintableCreator"
 import { FreeNFTDrop } from "../generated/schema";
 
-export function handleCreatedEdition(event: CreatedEditionEvent): void {
-  let receipt = event.receipt;
-  if (!receipt) {
-    log.warning("No receipt for event in tx {}", [event.transaction.hash.toHex()]);
-    return;
-  }
+function log_toString(log: ethereum.Log): string {
+  return log.topics[0].toHexString();
+}
 
-  let timeLimitSetMatches = receipt.logs.filter(log => log.topics[0] === Bytes.fromHexString("0x776e7116fa43d9385f3fd667b2e34d792fd3eec4b5a0aa8d1758b877ab4d6cbe"));
-  if (!timeLimitSetMatches || timeLimitSetMatches.length == 0) {
-    log.info("Skipping edition {}, no TimeLimitSet event in tx {}",
-    [event.params.editionContractAddress.toHexString(), event.transaction.hash.toHexString()]);
-    return;
-  }
+function logs_toString(logs: Array<ethereum.Log>): string {
+  return logs.map(log_toString).join(", ");
+}
 
-  let timeLimitSet = timeLimitSetMatches[0];
-
+function processTimeLimitSet(event: CreatedEditionEvent, timeLimitSet: ethereum.Log): void {
   // the parameters are not indexed, collection and deadline are packed into data, e.g.:
   // "data": "0x000000000000000000000000c39231fe8f61f1860c0cc0a9c8a016e46a8d0e7e0000000000000000000000000000000000000000000000000000000063435af8"
-  let collectionAddress = Address.fromString(timeLimitSet.data.toHexString().slice(2 + 24, 2 + 64));
-  if (collectionAddress !== event.params.editionContractAddress) {
+
+  // no 0x prefix
+  let collectionAddressStr = timeLimitSet.data.toHexString().slice(2 + 24, 2 + 64).toLowerCase();
+  if (!event.params.editionContractAddress.toHexString().toLowerCase().includes(collectionAddressStr)) {
     log.warning("In tx {}, collection address {} in TimeLimitSetEvent does not match editionContractAddress {} in CreatedEditionEvent",
-    [event.transaction.hash.toHexString(), collectionAddress.toHexString(), event.params.editionContractAddress.toHexString()]);
+    [event.transaction.hash.toHexString(), collectionAddressStr, event.params.editionContractAddress.toHexString()]);
     return;
   }
 
-  let entity = new FreeNFTDrop(collectionAddress);
+  let entity = new FreeNFTDrop(event.params.editionContractAddress);
   entity.creator = event.params.creator
   entity.editionSize = event.params.editionSize
-  entity.deadline = BigInt.fromUnsignedBytes(Bytes.fromHexString(timeLimitSet.data.toHexString().slice(2 + 64)));
+  entity.deadline = BigInt.fromUnsignedBytes(Bytes.fromHexString(timeLimitSet.data.toHexString().slice(2 + 64, 2 + 128)));
 
   // let edition = SingleEditionMintable.bind(collectionAddress);
   // entity.name = edition.name();
@@ -39,4 +34,27 @@ export function handleCreatedEdition(event: CreatedEditionEvent): void {
   // edition.imageUrl = edition.getURIs()[2];
 
   entity.save()
+}
+
+export function handleCreatedEdition(event: CreatedEditionEvent): void {
+  let receipt = event.receipt;
+  if (!receipt) {
+    log.critical("No receipt for event in tx {}", [event.transaction.hash.toHex()]);
+    return;
+  }
+
+  let txHash = event.transaction.hash.toHexString();
+
+  // checking all receipt.logs for a match on topics[0]
+  for (let i = 0; i < receipt.logs.length; i++) {
+    let someLog = receipt.logs[i];
+    let topic = log_toString(someLog);
+
+    if (topic.includes("0x776e7116fa43d9385f3fd667b2e34d792fd3eec4b5a0aa8d1758b877ab4d6cbe")) {
+      processTimeLimitSet(event, someLog);
+      return;
+    }
+  }
+
+  log.warning("No TimeLimitSet event in tx {}", [txHash]);
 }
